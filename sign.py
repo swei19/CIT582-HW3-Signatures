e71dfc4a417b3f0e4fd025f5e4e5b0c3f74f2924
SHENG-DONG WEI SHENG-DONG WEI <swei19@seas.upenn.edu>
2021-05-30 00:08:44 +0000

    updated

--- sign.py
+++ sign.py
@@ -7,18 +7,17 @@ from hashlib import sha256
 def sign(m):
 	#generate public key
 	#Your code here
-	private_key, public_key = keys.gen_keypair(secp256k1)
+    private_key, public_key = keys.gen_keypair(secp256k1)
 
 	#generate signature
 	#Your code here
-  
-  result = ecdsa.sign(msg = m, d = private_key, curve=secp256k1, hashfunc=sha256)
-  r = result[0]
-  s = result[1]
+    result = ecdsa.sign(msg = m, d = private_key, curve=secp256k1, hashfunc=sha256)
+    r = result[0]
+    s = result[1]
 
-	assert isinstance( public_key, point.Point )
-	assert isinstance( r, int )
-	assert isinstance( s, int )
-	return( public_key, [r,s] )
+    assert isinstance( public_key, point.Point )
+    assert isinstance( r, int )
+    assert isinstance( s, int )
+    return( public_key, [r,s] )
 
 
